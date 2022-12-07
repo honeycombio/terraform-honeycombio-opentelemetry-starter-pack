48f540e2edbced2ed78cd251f4eedef530ff733b
Davin Taddeo <davin@honeycomb.io>
2022-12-07 13:02:21 -0600

    hopefully fixing an issue where user turns off RPC references in queries
    Signed-off-by: Davin Taddeo <davin@honeycomb.io>

--- environment_derived_columns/protocols.tf
+++ environment_derived_columns/protocols.tf
@@ -22,7 +22,7 @@ resource "honeycombio_derived_column" "dc_protocols" {
   alias       = "dc_protocols"
   dataset     = "__all__"
   description = var.include_rpc_protocol_info_in_queries ? "Returns the RPC system used or what flavor of HTTP protocol used in a span" : "Returns the flavor of HTTP protocol used in a span"
-  expression  = templatefile("${path.module}/templates/protocols.tftpl", { query_rpc = var.include_rpc_protocol_info_in_queries })
+  expression  = templatefile("${path.module}/templates/protocols.tftpl", { query_rpc = var.include_rpc_protocol_info_in_queries, rpc_system_column = "$rpc.system" })
   depends_on  = [
     honeycombio_column.rpc-system,
     honeycombio_column.http-flavor,
