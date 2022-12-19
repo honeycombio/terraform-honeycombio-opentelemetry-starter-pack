d422e0660d7bfc8804fc7bcdde8e550833719ac1
Davin Taddeo <davin@honeycomb.io>
2022-12-19 13:15:44 -0600

    Update tests so they should always work even with terraform 0.12.0
    Signed-off-by: Davin Taddeo <davin@honeycomb.io>

--- tests/opentelemetry-starter-pack-test.tf
+++ tests/opentelemetry-starter-pack-test.tf
@@ -6,7 +6,6 @@ module "honeycombio-opentelemetry-starter-pack" {
   max_long_duration                    = 15000
   min_long_duration                    = 2000
   query_time_range                     = 604800
-  required_columns_dataset_name        = "-delete-me-later-"
 }
 
 output "all_services_board_url" {
