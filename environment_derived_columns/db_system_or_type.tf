e680eb1c2ec85ed0adfe005753b1c8ad2199972b
Davin Taddeo <davin@honeycomb.io>
2022-12-19 13:23:14 -0600

    hopefully the last fix I'll need, but we'll see
    Signed-off-by: Davin Taddeo <davin@honeycomb.io>

--- environment_derived_columns/db_system_or_type.tf
+++ environment_derived_columns/db_system_or_type.tf
@@ -6,10 +6,6 @@ resource "honeycombio_derived_column" "dc_db_system_or_type" {
   dataset     = "__all__"
   description = "Returns the database system if available or the database type if available"
   expression  = templatefile("${path.module}/templates/db_system_or_type.tftpl", {})
-  depends_on  = [
-    honeycombio_column.db-system,
-    honeycombio_column.db-type,
-  ]
 }
 
 ####################################################
